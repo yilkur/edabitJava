648d82effb6403c364ba2f580a360008ee35ba37
yilmazkurnazcan <yilmaz.kurnazcan@live.de>
2026-05-22 22:29:47 +0200

    solve Integer Digits Count

---
+++ IntegerDigitsCount/src/IntegerDigits.java
@@ -0,0 +1,5 @@
+public class IntegerDigits {
+    public static int count(int n) {
+        return Integer.toString(Math.abs(n)).length();
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println(VideoLengthInSeconds.minutesToSeconds("12:33"));
+        System.out.println(IntegerDigits.count(43342));
     }
 }
